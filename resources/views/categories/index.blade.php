56fbe6da8a1dabe0f4163e51d2905a675e40d604
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-09-01 01:09:50 -0300

    Ahora se pueden paginar las categorias y ver la cantidad de tareas por categoria

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,11 +6,14 @@
   </div>
   <div class="panel-body">
       <ul class="list-group">
+      @foreach ($categories as $category)
        <li class="list-group-item">
-          <a href="#" class="list-group-item"><span class="badge">13</span>
-          fffff</a>
+          <a href="{{route('category.show',$category)}}" class="list-group-item"><span class="badge">{{$cantTasks[$category->name]}}</span>
+          {{$category->name}}</a>
       </li>
+      @endforeach
     </ul>
+    {{ $categories->links() }}
   </div>
   <div class="panel-footer">
     <a href="{{route('tarea.index')}}" class="btn btn-default">Volver</a>
