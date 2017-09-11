24b377420efd6dcab0c54711104e1578e05c384d
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-09-11 18:32:28 -0300

    Se creo una policy para tareas

---
+++ app/Policies/TareaPolicy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Tareas;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TareaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the tareas.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tareas  $tareas
+     * @return mixed
+     */
+    public function view(User $user, Tareas $tareas)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create tareas.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+         if ($user->isManager{
+           return true;
+         }
+    }
+
+    /**
+     * Determine whether the user can update the tareas.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tareas  $tareas
+     * @return mixed
+     */
+    public function update(User $user, Tareas $tareas)
+    {
+
+    }
+
+    /**
+     * Determine whether the user can delete the tareas.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tareas  $tareas
+     * @return mixed
+     */
+    public function delete(User $user, Tareas $tareas)
+    {
+        //
+    }
+}
