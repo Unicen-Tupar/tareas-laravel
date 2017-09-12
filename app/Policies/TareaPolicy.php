5d91649d4018ebc2fc87c1b30999a649fbd98578
Gaston <gaston0403@gmail.com>
2017-09-12 17:19:44 -0300

    creamos la policy de usuario y agregamos una nueva columna de manager

---
+++ app/Policies/TareaPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Tarea;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TareaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the tarea.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tarea  $tarea
+     * @return mixed
+     */
+    public function view(User $user, Tarea $tarea)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create tareas.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the tarea.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tarea  $tarea
+     * @return mixed
+     */
+    public function update(User $user, Tarea $tarea)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the tarea.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Tarea  $tarea
+     * @return mixed
+     */
+    public function delete(User $user, Tarea $tarea)
+    {
+        //
+    }
+}
